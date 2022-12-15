69774ea9183707abf41137260deef9ad948c2d9b
abby-moana <abbymoana1@gmail.com>
2022-12-15 19:23:47 +0800

    making the class Queue iterable

--- Python/queues.py
+++ Python/queues.py
@@ -1,12 +1,20 @@
 from collections import deque
 
-class Queue:
-    def __init__(self):
-        self.elements = deque()
 
+class Queue:
+
+
+    def __init__(self, *elements):
+        self.elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
     def enqueue(self, element):
         self._elements.append(element)
 
     def dequeue(self):
         return self._elements.popleft()
-
