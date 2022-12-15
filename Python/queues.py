a2d6c7e44965293b56c0293de517a8c751264cf0
abby-moana <abbymoana1@gmail.com>
2022-12-16 01:21:09 +0800

    importing heappop and heappush

--- Python/queues.py
+++ Python/queues.py
@@ -1,5 +1,5 @@
 from collections import deque
-
+from heapq import heappop, heappush
 
 class Queue:
 
--- Python/stack.py
+++ Python/stack.py
@@ -1,3 +1,26 @@
-class Stack(Queue):
+from collections import deque
+
+
+class Queue:
+
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
     def dequeue(self):
-        return self._elements.pop
+        return self._elements.popleft()
+
+
+class Stack(Queue):
+
+    def dequeue(self):
+        return self._elements.pop()
