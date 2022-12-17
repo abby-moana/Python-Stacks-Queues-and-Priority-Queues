cec46f005d2213e0651da8fb1096d2a995a64dc6
abby-moana <abbymoana1@gmail.com>
2022-12-17 20:35:40 +0800

    import multiprocessing

--- Python/multiprocess_queue.py
+++ Python/multiprocess_queue.py
@@ -2,6 +2,7 @@ import time
 from hashlib import md5
 from itertools import product
 from string import ascii_lowercase
+import multiprocessing
 
 
 def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
