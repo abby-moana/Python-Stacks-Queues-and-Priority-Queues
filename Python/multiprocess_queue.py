854799cc6584d969509a9dca45123af7ceedd9b5
abby-moana <abbymoana1@gmail.com>
2022-12-17 20:20:41 +0800

    if statement

--- Python/multiprocess_queue.py
+++ Python/multiprocess_queue.py
@@ -9,3 +9,5 @@ def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
         for combination in product(alphabet, repeat=length):
             text_bytes = "".join(combination).encode("utf-8")
             hashed = md5(text_bytes).hexdigest()
+            if hashed == hash_value:
+                return text_bytes.decode("utf-8")
