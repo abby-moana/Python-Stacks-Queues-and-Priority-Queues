6103c7d3cafc6bc58527d5ae4931ff80531ad577
abby-moana <abbymoana1@gmail.com>
2022-12-17 21:17:21 +0800

    while loop

--- Python/producer.py
+++ Python/producer.py
@@ -5,3 +5,10 @@ QUEUE_NAME = "mailbox"
 with pika.BlockingConnection() as connection:
     channel = connection.channel()
     channel.queue_declare(queue=QUEUE_NAME)
+    while True:
+        message = input("Message: ")
+        channel.basic_publish(
+            exhange="",
+            routing_key=QUEUE_NAME,
+            body=message.encode("utf-8")
+        )
